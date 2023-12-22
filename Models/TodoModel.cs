8df77126898a1981006e5e4b4eb83749b6ff8a50
Cassio <cassiomariano774@gmail.com>
2023-12-21 22:43:20 -0300

    api

---
+++ Models/TodoModel.cs
@@ -0,0 +1,12 @@
+namespace Todo.Models
+{
+    public class TodoModel
+    {
+        public int Id { get; set; }
+        public String Title { get; set; }
+
+        public bool Done { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+    }
+}
